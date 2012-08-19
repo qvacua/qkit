55034c985aa4f90846bbb31c4e809ab1886b0834
Tae Won Ha <qvacua@gmail.com>
2012-08-19 11:12:30 +0200

    Initial Commit

---
+++ Qkit/Qkit.h
@@ -0,0 +1,13 @@
+//
+//  Qkit.h
+//  Qkit
+//
+//  Created by Tae Won Ha on 8/19/12.
+//  Copyright (c) 2012 Tae Won Ha. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Qkit : NSObject
+
+@end
---
+++ Qkit/Qkit.m
@@ -0,0 +1,13 @@
+//
+//  Qkit.m
+//  Qkit
+//
+//  Created by Tae Won Ha on 8/19/12.
+//  Copyright (c) 2012 Tae Won Ha. All rights reserved.
+//
+
+#import "Qkit.h"
+
+@implementation Qkit
+
+@end
